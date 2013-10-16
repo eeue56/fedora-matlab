76d5f19fd898751f3e0a7bec4a6394281803140e
root <root@FedoraMatlab.(none)>
2013-10-16 20:29:44 +0000

    supah commit dodalahdodado

--- php/feed.php
+++ php/feed.php
@@ -1,30 +1,25 @@
 <?php 
-include_once 'Feed_object.php';
 
-if($_SESSION['status'] == "authorized") {
+//if($_SESSION['status'] == "authorized") {
 
 	include_once "mysqlConnection.php";
 
 	try {
-	    var $result = $mysqli->execute("SELECT * FROM feed LIMIT 20 ORDER BY totalscore");
-	    var $html = "There are no feeds at this time"; 
-	    var $i = 0;
+	    $result = $mysqli->query("SELECT * FROM feed ORDER BY totalscore ASC LIMIT 20");
 
-	    while($row ->$result->fetch_assoc()) {
-	    	$dbField[$i] = $row;
-	    	$i++;
+	    $html = "There are no feeds at this time"; 
+
+	    if($result){
+	    	$html = '';
 	    }
 
-	    if($dbField){
-	    	$html = "";
-	    }
-	    
-	    foreach($dbField as $row => $cell) {
+	    while($row = $result->fetch_row()) {
+
 	    	$html .= "<div class='post'>";
-		    $html .= "<div class='upscore'>".$dbField[$row]['upscore']."</div>";
-	    	$html .= "<div class='username'>".$dbField[$row]['username']."</div>";
-		    $html .= "<div class='post_text'>".$dbField[$row]['post_text']."</div>";
-		    $html .= "<div class='downscore'>".$dbField[$row]['downscore']."</div>";
+		    $html .= "<div class='upscore'>".$row[3]."</div>";
+	    	$html .= "<div class='username'>".$row[1]."</div>";
+		    $html .= "<div class='post_text'>".$row[2]."</div>";
+		    $html .= "<div class='downscore'>".$row[4]."</div>";
 		    $html .= "</div>";
 	    }
 
@@ -36,6 +31,8 @@ if($_SESSION['status'] == "authorized") {
 	    echo "There are no feeds at this time";
 	    die();
     }
-}
+//}else{
+//	echo "login you tit.";
+//}
 
 ?>
--- php/mysqlConnection.php
+++ php/mysqlConnection.php
@@ -1,9 +1,8 @@
 <?php
 
-include_once("Logonconstants.php";)
+include_once "Logonconstants.php";
 
 	$mysqli = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME) or
 	die('There was a problem connecting to the database.');
-    }
 
 ?>
