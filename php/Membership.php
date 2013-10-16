3a2a796cb7415cae9a77b421e1bee3fc3a1744ae
root <root@FedoraMatlab.(none)>
2013-10-16 16:03:12 +0000

    added register method

--- php/Membership.php
+++ php/Membership.php
@@ -1,6 +1,7 @@
 <?php
 
 require 'LogonMysql.php';
+include_once "mysqlConnection.php";
 
 class Membership {
         
@@ -32,10 +33,31 @@ class Membership {
                 if($_SESSION['status'] !='authorized') header("location: login.php");
         }
 
-        
-        function userOptions() {
-                $mysql = New LogonMysql();
-                
+        function registerUser() {
+
+                $userdetails_json = $_POST['user_details']
+
+                $stmt = $mysqli->prepare('INSERT INTO members VALUES (?,?,?,?,?');
+
+                foreach($userdetails_json as $userdetail) {
+
+                        $username = $userdetail->username;
+                        $password = $userdetail->password;
+                        $email = $userdetail->email;
+                        $score = 0;
+                        $hats = '';
+
+                        $password = crypt($password, $username);
+
+                        $stmt->bind_param(sssib, $username, $password, $email, $score, $hats)
+
+                        $stmt->execute();
+                }
+
+
+                $stmt->close();
+
+
         }
         
-}
+};
