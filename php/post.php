cd3df66bc4d03e550d7012bbc7f7ba6fadec6349
root <root@FedoraMatlab.(none)>
2013-10-16 20:28:32 +0000

    uploadaboats

--- php/post.php
+++ php/post.php
@@ -1,23 +1,23 @@
 <?php
 include_once 'mysqlConnection.php';
+session_start();
 
        $post_json = $_POST['post_details'];
 
         $stmt = $mysqli->prepare("INSERT INTO feed VALUES (?,?,?,?,?,?)");
 
-        $post_details = json_decode($postdetails_json);
+        $post_details = json_decode($post_json);
 
         foreach($post_details as $postdetail) {
 
-
                 $feedid = uniqid();
-                $username = $postdetail->username;
-                $posttext = $postdetail->postext;
+                $username = 'test';
+                $posttext = $postdetail->posttext;
                 $upscore = 0;
                 $downscore = 0;
                 $total = 0;
 
-                $stmt->bind_param('sssiii', $username, $password, $email, $score, $hats);
+                $stmt->bind_param('sssiii', $feedid, $username, $posttext, $upscore, $downscore, $total);
 
                 $stmt->execute();
         }
