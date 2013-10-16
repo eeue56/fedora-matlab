38de7a40134cfdbc071b2eeaa700c78eacc39833
root <root@FedoraMatlab.(none)>
2013-10-16 16:46:28 +0000

    stuff for registering and shiz

---
+++ php/register.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/0.3.4/css/semantic.min.css"></link>
+    </head>
+    <body>
+
+    	<br />
+    	<br />
+
+    	<div class="ui form">
+    		<div class="field">
+    			<label>Username</label>
+    			<input type="text">
+    		</div>
+
+    		<div class="field">
+    			<label>Password</label>
+    			<input type="text">
+    		</div>
+
+
+    	</div>
+
+
+                <a type="submit" class="ui button" onClick="register_user()">Continue</a> <br>
+            </form>
+        </div>
+        <!--Load scripts-->
+        <script type="text/javascript" src="http://codeorigin.jquery.com/jquery-2.0.3.min.js"></script> 
+        <script type="text/javascript" src="../js/register.js"></script>     
+    </body>
+</html>
