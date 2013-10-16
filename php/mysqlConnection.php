bc377b656908ce23de75fc0762617fde676bd9ed
root <root@FedoraMatlab.(none)>
2013-10-16 16:01:06 +0000

    added register member method

---
+++ php/mysqlConnection.php
@@ -0,0 +1,9 @@
+<?php
+
+include_once("Logonconstants.php";)
+
+	$mysqli = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME) or
+	die('There was a problem connecting to the database.');
+    }
+
+?>
