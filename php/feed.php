393c99775b2b185b1970a6abae08ed1bdb6ea0d3
root <root@FedoraMatlab.(none)>
2013-10-16 16:10:41 +0000

    generate feed

---
+++ php/feed.php
@@ -0,0 +1,42 @@
+<?php 
+include_once 'Feed_object.php';
+
+if($_SESSION['status'] == "authorized") {
+
+	include_once "mysqlConnection.php";
+
+	try {
+	    var $result = $mysqli->execute("SELECT * FROM feed LIMIT 20");
+	    var $html = "There are no feeds at this time"; 
+	    var $i = 0;
+
+	    while($row ->$result->fetch_assoc()) {
+	    	$dbField[$i] = $row;
+	    	$i++;
+	    }
+
+	    if($dbField){
+	    	$html = "";
+	    }
+	    
+	    foreach($dbField as $row => $cell) {
+
+	    	$html .= "<div class='username'>".$dbField[$row]['username']."</div>";
+		    $html .= "<div class='post_text'>".$dbField[$row]['post_text']."</div>";
+		    $html .= "<div class='upscore'>".$dbField[$row]['upscore']."</div>";
+		    $html .= "<div class='downscore'>".$dbField[$row]['downscore']."</div>";
+
+
+	    }
+
+	    echo $html;
+	    
+	}
+	catch (Exception $e){
+	    error_log("it's broken");
+	    echo "There are no feeds at this time";
+	    die();
+    }
+}
+
+?>
